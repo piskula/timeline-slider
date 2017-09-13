0bb5443d238930f75caa1022af48b03131ca4cc1
Ondrej Oravcok <pista.baci@atlas.sk>
2017-09-13 21:16:40 +0200

    responsive scale - formatting scale according to major time units

--- src/app/timeline-frame/timeline-scale/timeline-scale.component.ts
+++ src/app/timeline-frame/timeline-scale/timeline-scale.component.ts
@@ -37,49 +37,67 @@ export class TimelineScaleComponent implements OnInit {
   refreshPipes() {
     const pipeInfo = this.getStep();
     const bigInterval = pipeInfo.bigInterval;
-    const bigIntervalAmount = ((this._max - this._min) / bigInterval) + 1;
-    const allIntervalAmount = ((bigIntervalAmount - 1) * (pipeInfo.smallInterval + 1)) + 1;
-    const smallInterval = bigInterval / (pipeInfo.smallInterval + 1);
+    let middleInterval: number;
+    let smallInterval: number;
 
-    const percentageLength = 100 / (allIntervalAmount - 1);
+    if (pipeInfo.smallIntervalAmount) {
+      smallInterval = (bigInterval / (pipeInfo.middleIntervalAmount + 1)) / (pipeInfo.smallIntervalAmount + 1);
+      middleInterval = bigInterval / (pipeInfo.middleIntervalAmount + 1);
+    } else {
+      smallInterval = bigInterval / (pipeInfo.middleIntervalAmount + 1);
+    }
+
     this.pipes = [];
 
-    console.log('bigInterval ' + bigInterval);
-    if (bigInterval < 600) { // not rounding to major timestamps
+    const majorMin = this._min % bigInterval === 0 ?
+      this._min :
+      this._min + (bigInterval - (this._min % bigInterval));
 
-      for (let i = 0; i <= allIntervalAmount - 1; i++) {
-        this.pipes.push(new Pipe(
-          (i * percentageLength) + '%',
-          (i * smallInterval) % bigInterval !== 0 ? '' : this.getPipeFormatted(this._min + (i * smallInterval), pipeInfo.pattern),
-          (i * smallInterval) % bigInterval !== 0 ? '8px' : '15px'
-        ));
+    let i = majorMin;
+    while (i <= this._max) {
+      let pipe: string;
+      let priority: number;
+      if (i % bigInterval === 0) {
+        pipe = this.getPipeFormatted(i, pipeInfo.pattern);
+        priority = 1;
+      } else if (middleInterval && i % middleInterval === 0) {
+        pipe = pipeInfo.subPattern ? this.getPipeFormatted(i, pipeInfo.subPattern) : '';
+        priority = 2;
+      } else {
+        pipe = '';
+        priority = 3;
       }
 
-    } else {  // rounding to major timestamps
-      const majorInterval = 3600;
-      const majorMin = this._min % majorInterval === 0 ?
-        this._min :
-        this._min + (majorInterval - (this._min % majorInterval));
+      this.pipes.push(new Pipe(
+        ((i - this._min) / (this._max - this._min)) * 100 + '%',
+        pipe,
+        priority
+      ));
+      i += smallInterval;
+    }
 
-      let i = majorMin;
-      while (i <= this._max) {
-        this.pipes.push(new Pipe(
-          ((i - this._min) / (this._max - this._min)) * 100 + '%',
-          i % majorInterval === 0 ? this.getPipeFormatted(i, pipeInfo.pattern) : '',
-          i % majorInterval === 0 ? '15px' : '8px'
-        ));
-        i += smallInterval;
+    i = majorMin - smallInterval;
+    while (i >= this._min) {
+
+      let pipe: string;
+      let priority: number;
+      if (i % bigInterval === 0) {
+        pipe = this.getPipeFormatted(i, pipeInfo.pattern);
+        priority = 1;
+      } else if (middleInterval && i % middleInterval === 0) {
+        pipe = this.getPipeFormatted(i, pipeInfo.subPattern);
+        priority = 2;
+      } else {
+        pipe = '';
+        priority = 3;
       }
 
-      i = majorMin - smallInterval;
-      while (i >= this._min) {
-        this.pipes.push(new Pipe(
-          ((i - this._min) / (this._max - this._min)) * 100 + '%',
-          i % majorInterval === 0 ? this.getPipeFormatted(i, pipeInfo.pattern) : '',
-          i % majorInterval === 0 ? '15px' : '8px'
-        ));
-        i -= smallInterval;
-      }
+      this.pipes.push(new Pipe(
+        ((i - this._min) / (this._max - this._min)) * 100 + '%',
+        pipe,
+        priority
+      ));
+      i -= smallInterval;
     }
   }
 
@@ -92,55 +110,61 @@ export class TimelineScaleComponent implements OnInit {
     const amount = this._max - this._min + 1;
     console.log('min ' + this._min + ', max ' + this._max + ', amount ' + amount);
     if (amount <= 11) {          // < 10 seconds  - interval 1 second
-      return new PipeInfo(1, 0, 'ss\'\'');                              // max 11
-    } else if (amount <= 21) {   // < 20 seconds  - interval 2 seconds
-      return new PipeInfo(2, 1, 'ss\'\'');                              // max 11
-    } else if (amount <= 61) {   // < 1 minute    - interval 5 seconds
-      return new PipeInfo(6, 5, 'ss\'\'');                              // max 13
+      return new PipeInfo(1, 0, null, 'ss\'\'', null);
+    } else if (amount <= 21) {   // < 20 seconds  - interval 5 seconds
+      return new PipeInfo(5, 4, null, 'ss\'\'', null);
+    } else if (amount <= 81) {   // < 1m 25s    - interval 10 seconds
+      return new PipeInfo(10, 1, 4, 'mm\' ss\'\'', 'ss\'\'');
     } else if (amount <= 121) {  // < 2 minutes   - interval 10 seconds
-      return new PipeInfo(10, 1, 'mm\' ss\'\'');                        // max 13
-    } else if (amount <= 241) {  // < 4 minutes   - interval 15 seconds
-      return new PipeInfo(15, 2, 'mm\' ss\'\'');                        // max 17
-    } else if (amount <= 301) {  // < 6 minutes   - interval 20 seconds
-      return new PipeInfo(20, 3, 'mm\' ss\'\'');                        // max 16
-    } else if (amount <= 421) {  // < 7 minutes   - interval 30 seconds
-      return new PipeInfo(30, 2, 'mm\' ss\'\'');                        // max 15
-    } else if (amount <= 661) {  // < 11 minutes  - interval 1 minute
-      return new PipeInfo(60, 5, 'mm\' ss\'\'');                        // max 13
-    } else if (amount <= 1021) {  // < 17 minutes  - interval 1m:30s
-      return new PipeInfo(90, 2, 'mm\' ss\'\'');                        // max 11
-    } else if (amount <= 1801) { // < 30 minutes  - interval 2 minutes
-      return new PipeInfo(120, 3, 'HH:mm');                             // max 11
-    } else if (amount <= 4501) { // < 75 minutes  - interval 5 minutes
-      return new PipeInfo(300, 4, 'HH:mm');
-    } else if (amount <= 7801) { // < 2h 10m      - interval 10 minutes
-      return new PipeInfo(600, 4, 'HH:mm');
+      return new PipeInfo(10, 1, 4, 'mm\' ss\'\'', null);
+    } else if (amount <= 241) {  // < 4 minutes   - interval 1 minute
+      return new PipeInfo(60, 3, 2, 'mm\' ss\'\'', 'ss\'\'');
+    } else if (amount <= 421) {  // < 7 minutes   - interval 1 minute
+      return new PipeInfo(60, 1, 9, 'mm\' ss\'\'', 'ss\'\'');
+    } else if (amount <= 601) {  // < 10 minutes   - interval 2 minutes
+      return new PipeInfo(120, 1, 9, 'mm\'', 'mm\'');
+    } else if (amount <= 901) {  // < 15 minutes  - interval 5 minutes
+      return new PipeInfo(300, 4, 9, 'mm\' ss\'\'', 'mm\'');
+    } else if (amount <= 1201) {  // < 20 minutes  - interval 5 minutes
+      return new PipeInfo(300, 4, 5, 'mm\'', 'mm\'');
+    } else if (amount <= 2701) { // < 45 minutes  - interval 5 minutes
+      return new PipeInfo(300, 4, null, 'HH:mm', null);
+    } else if (amount <= 5101) { // < 85 minutes  - interval 10 minutes
+      return new PipeInfo(600, 1, 4, 'HH:mm', 'mm\'');
+    } else if (amount <= 9001) { // < 2h 30m      - interval 30 minutes
+      return new PipeInfo(1800, 2, 9, 'HH:mm', 'mm\'');
+    } else if (amount <= 30001) { // < 2h 10m      - interval 1 hour
+      return new PipeInfo(3600, 3, 2, 'HH:mm', 'mm\'');
     }
-    return new PipeInfo(3600, 3, 'HH:mm');
+    // TODO until years
+    return new PipeInfo(1800, 4, 2, 'HH:mm', null);
   }
-
 }
 
 class PipeInfo {
   bigInterval: number;
-  smallInterval: number;
+  smallIntervalAmount: number;
+  middleIntervalAmount: number;
   pattern: string;
+  subPattern: string;
 
-  constructor(bigInterval: number, smallInterval: number, pattern: string) {
+  constructor(bigInterval: number, middleIntervalAmount: number, smallIntervalAmount: number, pattern: string, subPattern: string) {
     this.bigInterval = bigInterval;
-    this.smallInterval = smallInterval;
+    this.middleIntervalAmount = middleIntervalAmount;
+    this.smallIntervalAmount = smallIntervalAmount;
     this.pattern = pattern;
+    this.subPattern = subPattern;
   }
 }
 
 class Pipe {
   value: string;
   title: string;
-  height: string;
+  priority: number;
 
-  constructor(value: string, title: string, height: string) {
+  constructor(value: string, title: string, priority: number) {
     this.value = value;
     this.title = title;
-    this.height = height;
+    this.priority = priority;
   }
 }
