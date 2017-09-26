77470ade94a05e89e420df69eefd124537779c35
Ondrej Oravcok <pista.baci@atlas.sk>
2017-09-26 11:54:47 +0200

    wrong path - version fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TimelineSliderComponent } from './timeline-frame/timeline-slider/timeline-slider.component';
-import { NouisliderModule } from 'ng2-nouislider';
+import { NouisliderModule } from 'ng2-nouislider/src/nouislider';
 import { TimelineScaleComponent } from './timeline-frame/timeline-scale/timeline-scale.component';
 import { TimelineFrameComponent } from './timeline-frame/timeline-frame.component';
 import { TimeLabelComponent } from './timeline-frame/time-label/time-label.component';
