8d75a16980ca0e607807eb9005ca0cb80d4e4c73
Ondrej Oravcok <pista.baci@atlas.sk>
2017-12-06 20:18:28 +0100

    forgottrn code removed (NodeJS cache error)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { D3SliderDirective } from './slider-directive/slider.directive';
-// import { D3SliderDirective } from 'ng-d3-slider/d3-slider.directive';
 
 import { AppComponent } from './app.component';
+import { ShareTimeService } from './time-service/share-time.service';
 import { TimelineSliderLowerComponent } from './timeline-frame/timeline-slider-lower/timeline-slider-lower.component';
-import { NouisliderModule } from 'ng2-nouislider/src/nouislider';
 import { TimelineScaleComponent } from './timeline-frame/timeline-scale/timeline-scale.component';
 import { TimelineFrameComponent } from './timeline-frame/timeline-frame.component';
 import { TimeLabelComponent } from './timeline-frame/time-label/time-label.component';
 import { TimelineSliderUpperComponent } from './timeline-frame/timeline-slider-upper/timeline-slider-upper.component';
 import { TimelineLockComponent } from './timeline-frame/timeline-slider-upper/timeline-lock/timeline-lock.component';
-import {ShareTimeService} from "./time-service/share-time.service";
 
 @NgModule({
   declarations: [
@@ -31,7 +29,6 @@ import {ShareTimeService} from "./time-service/share-time.service";
     BrowserModule,
     FormsModule,
     HttpModule,
-    NouisliderModule
   ],
   providers: [ShareTimeService],
   bootstrap: [AppComponent]
