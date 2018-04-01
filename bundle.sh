8a45014934ae8bf0ebabb40af15e46bf06fa1ff2
Ondrej Oravcok <pista.baci@atlas.sk>
2018-04-02 00:02:14 +0200

    fixed portlet build with font-awesome

--- bundle.sh
+++ bundle.sh
@@ -4,7 +4,7 @@ STATUS=0;
 
 echo "Building Angular application"
 cd angular
-npm install && ng build --prod
+npm install && ng build --prod -vc=true
 
 STATUS=$?
 
@@ -13,17 +13,18 @@ then
   printf '\u274c Failed\n'
   exit 1
 else
+  # clear JS and CSS
+  echo '' > ../src/main/webapp/js/main.js
+  echo '' > ../src/main/webapp/css/main.css
+
   echo "Merging and copying files"
   cat dist/inline.*.bundle.js \
       dist/polyfills.*.bundle.js \
       dist/vendor.*.bundle.js \
       dist/main.*.bundle.js > ../src/main/webapp/js/main.js && \
   cat dist/styles.*.bundle.css > ../src/main/webapp/css/main.css
+  cp -v dist/fontawesome-webfont.* ../src/main/webapp/css/
   STATUS=$?
-
-  # clear JS and CSS
-  # echo '' > ../src/main/webapp/js/main.js
-  # echo '' > ../src/main/webapp/css/main.css
 fi
 
 
